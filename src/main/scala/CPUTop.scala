b3334072ac5205c225e6f8edb03d8f02212c1c65
Victor Clausen <s232604@dtu.dk>
2024-11-13 13:47:13 +0100

    små ændringer i control unit

--- src/main/scala/CPUTop.scala
+++ src/main/scala/CPUTop.scala
@@ -3,23 +3,23 @@ import chisel3.util._
 
 class CPUTop extends Module {
   val io = IO(new Bundle {
-    val done = Output(Bool ())
-    val run = Input(Bool ())
-    //This signals are used by the tester for loading and dumping the memory content, do not touch
-    val testerDataMemEnable = Input(Bool ())
-    val testerDataMemAddress = Input(UInt (16.W))
-    val testerDataMemDataRead = Output(UInt (32.W))
-    val testerDataMemWriteEnable = Input(Bool ())
-    val testerDataMemDataWrite = Input(UInt (32.W))
-    //This signals are used by the tester for loading and dumping the memory content, do not touch
-    val testerProgMemEnable = Input(Bool ())
-    val testerProgMemAddress = Input(UInt (16.W))
-    val testerProgMemDataRead = Output(UInt (32.W))
-    val testerProgMemWriteEnable = Input(Bool ())
-    val testerProgMemDataWrite = Input(UInt (32.W))
+    val done = Output(Bool())
+    val run = Input(Bool())
+    // This signals are used by the tester for loading and dumping the memory content, do not touch
+    val testerDataMemEnable = Input(Bool())
+    val testerDataMemAddress = Input(UInt(16.W))
+    val testerDataMemDataRead = Output(UInt(32.W))
+    val testerDataMemWriteEnable = Input(Bool())
+    val testerDataMemDataWrite = Input(UInt(32.W))
+    // This signals are used by the tester for loading and dumping the memory content, do not touch
+    val testerProgMemEnable = Input(Bool())
+    val testerProgMemAddress = Input(UInt(16.W))
+    val testerProgMemDataRead = Output(UInt(32.W))
+    val testerProgMemWriteEnable = Input(Bool())
+    val testerProgMemDataWrite = Input(UInt(32.W))
   })
 
-  //Creating components
+  // Creating components
   val programCounter = Module(new ProgramCounter())
   val dataMemory = Module(new DataMemory())
   val programMemory = Module(new ProgramMemory())
@@ -27,21 +27,52 @@ class CPUTop extends Module {
   val controlUnit = Module(new ControlUnit())
   val alu = Module(new ALU())
 
-  //Connecting the modules
-  //programCounter.io.run := io.run
-  //programMemory.io.address := programCounter.io.programCounter
+  // Connecting the modules
+  programCounter.io.run := io.run
+  programMemory.io.address := programCounter.io.programCounter
 
-  ////////////////////////////////////////////
-  //Continue here with your connections
-  ////////////////////////////////////////////
+  // Fetch instruction from program memory
+  val instruction = programMemory.io.instructionRead
 
-  //This signals are used by the tester for loading the program to the program memory, do not touch
+  // Decode instruction
+  controlUnit.io.opcode := instruction(31, 26)
+
+  // Register file connections
+  registerFile.io.aSel := instruction(25, 21)
+  registerFile.io.bSel := instruction(20, 16)
+  registerFile.io.writeSel := instruction(15, 11)
+  registerFile.io.writeEnable := controlUnit.io.regWrite
+
+  // Introduce a register to break the combinational loop
+  val dataReadReg = RegNext(dataMemory.io.dataRead)
+  registerFile.io.writeData := Mux(controlUnit.io.memRead, dataReadReg, alu.io.output)
+
+  // ALU connections
+  alu.io.input1 := registerFile.io.a
+  alu.io.input2 := Mux(controlUnit.io.aluSrc, instruction(15, 0).asUInt, registerFile.io.b)
+  alu.io.sel := controlUnit.io.aluOp
+
+  // Data memory connections
+  dataMemory.io.address := alu.io.output
+  dataMemory.io.dataWrite := registerFile.io.b
+  dataMemory.io.writeEnable := controlUnit.io.memWrite
+
+  // Program counter connections
+  programCounter.io.stop := controlUnit.io.end
+  programCounter.io.jump := controlUnit.io.jump
+  programCounter.io.programCounterJump := instruction(15, 0).asUInt
+
+  // Done signal
+  io.done := controlUnit.io.end
+
+  // This signals are used by the tester for loading the program to the program memory, do not touch
   programMemory.io.testerAddress := io.testerProgMemAddress
   io.testerProgMemDataRead := programMemory.io.testerDataRead
   programMemory.io.testerDataWrite := io.testerProgMemDataWrite
   programMemory.io.testerEnable := io.testerProgMemEnable
   programMemory.io.testerWriteEnable := io.testerProgMemWriteEnable
-  //This signals are used by the tester for loading and dumping the data memory content, do not touch
+
+  // This signals are used by the tester for loading and dumping the data memory content, do not touch
   dataMemory.io.testerAddress := io.testerDataMemAddress
   io.testerDataMemDataRead := dataMemory.io.testerDataRead
   dataMemory.io.testerDataWrite := io.testerDataMemDataWrite
--- src/main/scala/RegisterFile.scala
+++ src/main/scala/RegisterFile.scala
@@ -11,24 +11,19 @@ class RegisterFile extends Module {
     val writeSel =Input(UInt(4.W)) //register som skal skrives i
     val writeEnable = Input(Bool()) //true hvis der skal skrives i en register
     val writeData = Input(UInt(32.W)) //lortet som skal skrives i den valgte register
-
-    val reg= RegInit(0.U(32.W)) //just set it to 32 bits, could make it smaller :/
-    val enableReg= Reg(UInt(4.W))
-
-    val cntReg= RegInit(0.U(4.W))
-
     val a = Output(UInt(32.W))
     val b =Output(UInt(32.W))
   })
+  io.a:=0.U
+  io.b:=0.U
 
-  //Implement this module here
-
-  val registerFile = RegInit(VecInit(Seq.fill(16)(0.U(32.W)))) //16 registers, 32 bits each.
+  //val registerFile = RegInit(VecInit(Seq.fill(32)(0.U(32.W)))) //32 registers, 32 bits each.
   //all starting with value 0.
+  val registers = RegInit(VecInit(Seq.fill(32)(0.U(32.W))))
 
   when(io.writeEnable){
-    registerFile(io.writeSel):=io.writeData
+    registers(io.writeSel):=io.writeData
   }
-  io.a := registerFile(io.aSel)
-  io.b := registerFile(io.bSel)
+  io.a := registers(io.aSel)
+  io.b := registers(io.bSel)
 }
